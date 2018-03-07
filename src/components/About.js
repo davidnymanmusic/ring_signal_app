29f35f47d6bfcbb6592b6eae8082df17e9ff0aae
David Nyman <davenyman@gmail.com>
2018-03-07 09:48:37 -0500

    fix css display

--- src/components/About.js
+++ src/components/About.js
@@ -6,10 +6,10 @@ const About = () =>
   <h3>more info</h3>
 
 <p className='instructions'>Q3 project for Galvanize Web Development Immersive</p>
-<a href="https://github.com/davidnymanmusic/">GitHub</a>
-<a href="https://twitter.com/davidnyman">twitter</a>
-<a href="davidnymanmusic.github.io/white-noise/noise.html">Q1: White Noise</a>
-<a href="https://dogeat.herokuapp.com">Q2: Dog Eat ___?</a>
+<a className='links' href="https://github.com/davidnymanmusic/">GitHub</a>
+<a className='links' href="https://twitter.com/davidnyman">twitter</a>
+<a className='links' href="davidnymanmusic.github.io/white-noise/noise.html">Q1: White Noise</a>
+<a className='links' href="https://dogeat.herokuapp.com">Q2: Dog Eat ___?</a>
 
 </div>
 
