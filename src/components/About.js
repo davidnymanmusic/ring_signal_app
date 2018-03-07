aadc2b7f086ec730772b1772798e092511204fa7
David Nyman <davenyman@gmail.com>
2018-03-07 09:58:26 -0500

    fix q1 link

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ const About = () =>
 <p className='instructions'>Q3 project for Galvanize Web Development Immersive</p>
 <a className='links' href="https://github.com/davidnymanmusic/">GitHub</a>
 <a className='links' href="https://twitter.com/davidnyman">twitter</a>
-<a className='links' href="davidnymanmusic.github.io/white-noise/noise.html">Q1: White Noise</a>
+<a className='links' href="https://davidnymanmusic.github.io/white-noise/noise.html">Q1: White Noise</a>
 <a className='links' href="https://dogeat.herokuapp.com">Q2: Dog Eat ___?</a>
 
 </div>
