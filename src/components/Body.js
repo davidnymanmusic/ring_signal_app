491bea3acdd1823d78a2834da373518714018a8f
David Nyman <davenyman@gmail.com>
2018-03-08 13:32:54 -0500

    fix typo

--- src/components/Body.js
+++ src/components/Body.js
@@ -15,7 +15,7 @@ const Body = ({title = 'cool title'}) =>
   <ul>
     <li className="nav-link"><Link to='/install'>install</Link></li>
     <li className="nav-link"><Link to='/alarms'>alarms</Link></li>
-    <li className="nav-link"><Link to='/notifcation'>notifcation</Link></li>
+    <li className="nav-link"><Link to='/notification'>notification</Link></li>
     <li><Link to='/about'>about</Link></li>
   </ul>
 </div>
@@ -25,7 +25,7 @@ const Body = ({title = 'cool title'}) =>
   <Route path='/install' component={Install}/>
   <Route path='/about' component={About}/>
   <Route path='/alarms' component={Alarms}/>
-  <Route path='/notifcation' component={Notification}/>
+  <Route path='/notification' component={Notification}/>
   <Route path='/' component={Home}/>
 </Switch>
 </div>
